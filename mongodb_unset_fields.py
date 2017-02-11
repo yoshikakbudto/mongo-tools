ee03efb17d0f011b1ee73e3b58980a082391a27c
yoshi kakbudto <dmitry.esv@gmail.com>
2017-02-12 03:35:29 +0500

    release 0.1

--- mongodb_unset_fields.py
+++ mongodb_unset_fields.py
@@ -4,14 +4,21 @@
 """
 removes meta field from documents with ObjectIds given in OBJ_IDS_FIE
 
-the list generated based on the log of mongo --eval "db.getMongo().setSlaveOk(); db.upgradeCheckAllDBs();" db06av-ru.psc/admin > /tmp/upgradecheckalldbs.txt
+the list generated based on the log of 
+    mongo --eval "db.getMongo().setSlaveOk(); db.upgradeCheckAllDBs();" db06av-ru.psc/admin > /tmp/upgradecheckalldbs.txt
 sed -nE 's/(.*ObjectId..)([[:alnum:]]+)(.*)/\2/p' /tmp/upgradecheckalldbs_builduser.txt
 
 """
 
 import os
-import pymongo
+import pprint
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
+MONGO_HOST = 'db04av-ru.psc'
+MONGO_PORT = 27017
+MONGO_DB = 'cosmosim_pubtest'
+MONGO_COLLECTION = 'freetopayQueue'
 OBJ_IDS_FILE = 'object_ids.lst'
 
 of = os.path.dirname(os.path.realpath(__file__)) + r'/' + OBJ_IDS_FILE
@@ -20,5 +27,12 @@ with open(of, 'r') as f:
     for line in f.xreadlines():
         objids.append(line.strip())
 
+with MongoClient(MONGO_HOST, MONGO_PORT, maxPoolSize=3) as c:
+    db = c[MONGO_DB]
+    col = db[MONGO_COLLECTION]
+    # pprint.pprint(col.find_one({"_id" : ObjectId("558d77ce2f410a101e293a5a")}))
+    for objid in objids:    
+        print "removing meta field from doc id %s" % objid
+        col.update({'_id': ObjectId(objid)}, {'$unset':{'meta':1}})
 # print(objids)
 
